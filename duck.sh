50bc542da33b6e0d2097cfd8459ba93a072853a4
Anuj Datar <anuj.datar@gmail.com>
2021-03-08 15:22:51 +0530

    Added base code for duckdns ip refresh based on docs

--- duck.sh
+++ duck.sh
@@ -1,1 +1,45 @@
-echo url="https://www.duckdns.org/update?domains=exampledomain&token=a7c4d0ad-114e-40ef-ba1d-d217904a50f2&ip=" | curl -k -o ~/duckdns/duck.log -K -
+#!/bin/sh
+
+if [ -z $SUBDOMAINS ]; then
+  if [ -z $SUBDOMAINS_FILE ]; then
+    echo 'Please enter DuckDNS Subdomains as an environment variable or secret'
+    exit 1
+  else
+    if [ -f $SUBDOMAINS_FILE ]; then
+      # echo 'subdomains in secret file'
+      duck_subdomain="$(cat ${SUBDOMAINS_FILE})"
+    else
+      echo "Please check subdomains secret. Passed incorrectly."
+      exit 1
+    fi
+  fi
+else
+  duck_subdomain=$SUBDOMAINS
+fi
+
+if [ -z $TOKEN ]; then
+  if [ -z $TOKEN_FILE ]; then
+    echo 'Please enter $DUCKDNS_TOKEN as an environment variable or secret'
+    exit 1
+  else
+    if [ -f $TOKEN_FILE ]; then
+      # echo 'token in secret file'
+      duck_token="$(cat ${TOKEN_FILE})"
+    else
+      echo "Please check token secret. Passed incorrectly"
+      exit 1
+    fi
+  fi
+else
+  duck_token=$TOKEN
+fi
+
+# curl "https://www.duckdns.org/update?domains=${duck_subdomain}&token=${duck_token}&ip="
+
+info=$(echo url="https://www.duckdns.org/update?domains=${duck_subdomain}&token=${duck_token}&ip=" | curl -s -k -K - )
+
+if [ "$info" == "OK" ]; then
+  echo "[$(date)] DuckDNS update ok"
+else
+  echo "[$(date)] DuckDNS update failed"
+fi
