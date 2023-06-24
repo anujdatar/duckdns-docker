98854bfb0d18ed86f833bb93258f25bd8aa42f14
Anuj Datar <anuj.datar@gmail.com>
2023-06-24 16:44:33 +0530

    Fix: if-check for secrets files causing errors because of missing quotes
    github copilot auto-completion errors. error was showing up when using e-vars directly

--- scripts/container-setup.sh
+++ scripts/container-setup.sh
@@ -6,7 +6,7 @@ print_breaker() {
 
 # #####################################################################
 # Step 1: set up timezone
-if [ -z $TZ ]; then
+if [ -z "$TZ" ]; then
   echo "TZ environment variable not set. Using default: UTC"
 else
   echo "Setting timezone to $TZ"
@@ -18,20 +18,20 @@ echo "Starting DuckDNS container: [$(date)]"
 print_breaker
 # #####################################################################
 # Step 2: Check API Key
-if [ -f $TOKEN_FILE ]; then
+if [ -f "$TOKEN_FILE" ]; then
   TOKEN=$(cat "$TOKEN_FILE")
 fi
-if [ -z $TOKEN ]; then
+if [ -z "$TOKEN" ]; then
   echo "Please enter a valid TOKEN env variable or TOKEN_FILE secret"
   exit 1
 fi
 echo "DuckDNS Token  ---  OK"
 # #####################################################################
 # Step 3: Check Subdomains
-if [ -f $SUBDOMAINS_FILE ]; then
+if [ -f "$SUBDOMAINS_FILE" ]; then
   SUBDOMAINS=$(cat "$SUBDOMAINS_FILE")
 fi
-if [ -z $SUBDOMAINS ]; then
+if [ -z "$SUBDOMAINS" ]; then
   echo "Please enter a valid SUBDOMAINS env variable or SUBDOMAINS_FILE secret"
   exit 1
 fi
@@ -39,8 +39,8 @@ echo "DuckDNS Subdomains  ---  OK"
 # #####################################################################
 # Step 4: Save to config file
 print_breaker
-echo "TOKEN=$TOKEN" > /config.sh
-echo "SUBDOMAINS=$SUBDOMAINS" >> /config.sh
+echo "TOKEN=\"$TOKEN\"" > /config.sh
+echo "SUBDOMAINS=\"$SUBDOMAINS\"" >> /config.sh
 # #####################################################################
 echo "DuckDNS container setup complete"
 print_breaker
