263e0b2bd2c08799ed1b157fa0f0a0657ea5cc0b
Anuj Datar <anuj.datar@gmail.com>
2023-06-28 19:09:07 +0530

    restructured rest of the update script to log only one line each update

--- scripts/ddns-update.sh
+++ scripts/ddns-update.sh
@@ -34,51 +34,32 @@ get_ip6() {
 }
 # #####################################################################
 # Step 1: get public IP address
-echo Fetching record type: $RECORD_TYPE
 if [ "$RECORD_TYPE" == "A" ]; then
-	CURRENT_IP=$(curl -s https://api.ipify.org || curl -s https://ipv4.icanhazip.com/)
-
-	# check cloudflare's dns server if above method doesn't work
-	if [ -z $CURRENT_IP ]; then
-		echo using cloudflare whoami to find ip
-    CURRENT_IP=$(dig txt ch +short whoami.cloudflare @1.1.1.1 | tr -d '"')
-	fi
+  CURRENT_IP=$(get_ip4)
+  PROTO="ip"
 elif [ "$RECORD_TYPE" == "AAAA" ]; then
-	CURRENT_IP=$(curl -s https://api6.ipify.org || curl -s https://ipv6.icanhazip.com/)
-
-	# check cloudflare's dns server if above method doesn't work
-	if [ -z $CURRENT_IP ]; then
-		echo using cloudflare whoami to find ip
-    CURRENT_IP=$(dig txt ch +short whoami.cloudflare @2606:4700:4700::1111 | tr -d '"')
-	fi
+  CURRENT_IP=$(get_ip6)
+  PROTO="ipv6"
 fi
 
 if [ -z $CURRENT_IP ]; then
     echo "No public IP found: check internet connection or network settings"
     exit 1
 fi
-echo "Current time: [$(date)]"
-echo "Current Public IP: $CURRENT_IP"
-
 # #####################################################################
-# Step 2: Update ddns
+# Step 2: check against old ip
 OLD_IP=$(cat /old_record_ip)
-echo "Stored IP address: $OLD_IP"
 if [ "$OLD_IP" == "$CURRENT_IP" ]; then
-    echo "IP address has not changed. Update not required"
-    print_breaker
+    echo "[$(date)]: IP unchanged, not updating. IP: $CURRENT_IP"
     exit 0
 fi
-echo "IP address has changed. Updating DuckDNS..."
-if [ "$RECORD_TYPE" == "A" ]; then
-  update=$(curl -s "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ip=${CURRENT_IP}")
-elif [ "$RECORD_TYPE" == "AAAA" ]; then
-  update=$(curl -s "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ipv6=${CURRENT_IP}")
-fi
+# #####################################################################
+# Step 3: Update ddns
+update=$(curl -s "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&${PROTO}=${CURRENT_IP}")
 
 if [ "$update" == "OK" ]; then
-  echo "DNS Record for $SUBDOMAINS successfully updated to: $CURRENT_IP"
+  echo "[$(date)]: DuckDNS update successful.  IP: $CURRENT_IP"
   echo $CURRENT_IP > /old_record_ip
 else
-  echo "DuckDNS update failed"
+  echo "[$(date)]: DuckDNS update failed. Curr IP: $CURRENT_IP"
 fi
